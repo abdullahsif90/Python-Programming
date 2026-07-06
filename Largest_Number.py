3344793f3a08e64dfd33be64170d2a5014e79c19
Abdullah Asif <abdullahasif8639260@gmail.com>
2026-07-06 22:24:21 +0500

    Comparison of two numbers
    This programm asks from user to enter the two numbers and then compare which number is greater , then return the greatest the number

---
+++ Largest_Number.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter the 1st number: "))
+num2=int(input("Enter the 2nd number: "))
+if(num1>num2):
+    print("The number ",num1," is greater")
+else:
+    print("The number ",num2," is greater")

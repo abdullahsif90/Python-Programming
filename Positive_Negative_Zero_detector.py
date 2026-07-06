a89d2fd8bc99377ea2d7a47b73a772a46d8d1e8b
Abdullah Asif <abdullahasif8639260@gmail.com>
2026-07-06 22:16:48 +0500

    Positive , Negative and Zero number Detector
    This progam tell the number you Entered is Negative, zero or positive using If-Else Statement

---
+++ Positive_Negative_Zero_detector.py
@@ -0,0 +1,7 @@
+number= int(input("Enter the Number: "))
+if(number>0):
+    print("The Number is positive ")
+elif(number<0):
+    print("The number is Negative ")
+else:
+    print("The Number you entered is 0 ")

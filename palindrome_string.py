dc901ba3e1bfe2931fd7dbcdf57550086e5e6d72
Abdullah Asif <abdullahasif8639260@gmail.com>
2026-07-07 23:42:40 +0500

    Palindrome Checker
    This programm tells the word or number is palindrome or not 
    this program uses the slicing method to work

---
+++ palindrome_string.py
@@ -0,0 +1,10 @@
+print("Palindrome Checker")
+
+word = input("Enter a word: ")
+
+reverse_word = word[::-1]
+
+if word == reverse_word:
+    print("The word is a palindrome.")
+else:
+    print("The word is not a palindrome.")

c785a7d13b910ed6236a9cfac53b327ec6623ec4
Abdullah Asif <abdullahasif8639260@gmail.com>
2026-07-08 23:35:30 +0500

    Student information system using dictionary
    Created a student information system using Python dictionaries. The program takes user input, stores student details in a dictionary, and displays the information in a structured format.

---
+++ student_informtion_system.py
@@ -0,0 +1,23 @@
+# print("Student Information System")
+# data={}
+# x1= input("Enter the name: ")
+# x2=int(input("Enter the age: "))
+# x3=input("Enter the University Name: ")
+# x4= input("Enter the Registration Number: ")
+# x5=int(input("Enter the Semester: "))
+# x6=float(input("Enter the CGPA: "))
+# data.update({"Name: ":x1})
+# data.update({"Age: ":x2})
+# data.update({"University: ":x3})
+# data.update({"Registration Number: ":x4})
+# data.update({"Semester: ":x5})
+# data.update({"CGPA: ":x6})
+# print(data)
+marks={}
+x1=input("Enter name ")
+x2=input("age: ")
+x3=input("city: ")
+marks["name"] = x1
+marks["age"] = x2
+marks["city"] = x3
+print(marks)

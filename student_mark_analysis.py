35a8d82655485d5909237bb1982cec846878cb75
Abdullah Asif <abdullahasif8639260@gmail.com>
2026-07-09 13:11:31 +0500

    Add student marks analysis
    Analyze student marks using Python list and built-in functions to calculate the highest marks, lowest marks, total marks, and average marks.

---
+++ student_mark_analysis.py
@@ -0,0 +1,13 @@
+marks=[78,97,56,67,85,94]
+
+print("The marks are: ",marks)
+
+print("The total marks are:", sum(marks)) 
+
+print("The minimun marks are: ",min(marks))
+
+print("The highest marks are: ",max(marks))
+
+average=sum(marks)/len(marks)
+
+print("The average marks are: ",average)

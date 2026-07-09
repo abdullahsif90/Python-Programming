2a1d464ed87648fe35b443a34724fa85165d1dfa
Abdullah Asif <abdullahasif8639260@gmail.com>
2026-07-09 12:35:44 +0500

    Add shopping cart management using Python list
    Created a shopping cart management program using Python lists. The program stores shopping items, displays the total number of items, shows the first and last item, and performs add and remove operations using list methods.

---
+++ shopping_cart.py
@@ -0,0 +1,22 @@
+# shopping_cart = ["Eggs","Milk","Bread","Rice","Juice"]
+# print("The shopping list is: ",shopping_cart)
+# print("Total items are: ",len(shopping_cart))
+# print("The first item is: ",shopping_cart[0])
+# print("The last item is: ",shopping_cart[4])
+# shopping_cart.append("Butter")
+# print("The item after adding is: ",shopping_cart)
+# shopping_cart.remove("Rice")
+# print("The item after removing is: ",shopping_cart)
+
+#  shopping cart
+shopping_cart=[]
+x1=input("Enter the first item: ")
+x2=input("Enter the second item: ")
+x3=input("Enter the third item: ")
+x4=input("Enter the fourth item: ")
+shopping_cart.append(x1)
+shopping_cart.append(x2)
+shopping_cart.append(x3)
+shopping_cart.append(x4)
+print("The items are: ", shopping_cart)
+print(shopping_cart[-1])

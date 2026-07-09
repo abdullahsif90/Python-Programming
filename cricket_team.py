fd4dcd1109e2b2b647a1ccba5aad15c9f500c464
Abdullah Asif <abdullahasif8639260@gmail.com>
2026-07-09 14:05:02 +0500

    Add cricket team tuple program
    Create a cricket team using a Python tuple and demonstrate tuple operations including indexing, length, count, and index methods.

---
+++ cricket_team.py
@@ -0,0 +1,15 @@
+team = ("Rizwan","Babar","Shaheen","Haris","Naseem")
+
+print("The Cricket team is: ",team)
+
+print("The length of team is: ",len(team))
+
+print("First Player:",team[0])
+
+print("Last Player:",team[-1])
+
+print("The index number of Shaheen is: ",team.index("Shaheen"))
+
+print("Babar appears:",team.count("Babar"),"times")
+
+

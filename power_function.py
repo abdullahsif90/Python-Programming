55c8a3a9a871d4c7ab2271745fe21ac46de89198
Abdullah Asif <abdullahasif8639260@gmail.com>
2026-07-15 23:42:27 +0500

    Implement power calculator using functions
    Created a Python program that calculates the power of a number using a function. The program accepts the base number and exponent from the user and computes the result using the exponent (**) operator.

---
+++ power_function.py
@@ -0,0 +1,8 @@
+def power(number, exponent):
+    return number ** exponent
+
+number = int(input("Enter the number: "))
+exponent =int(input("Enter the exponent: "))
+
+result =power(number, exponent)
+print("Result: ",result)

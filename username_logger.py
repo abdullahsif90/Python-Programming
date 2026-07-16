ee44ddd6a24211d06883d6bbdf0485340aa40a3c
Abdullah Asif <abdullahasif8639260@gmail.com>
2026-07-16 22:38:20 +0500

    Enable logging of multiple usernames
    Modified the username logger to accept multiple usernames and save them to the file.

--- username_logger.py
+++ username_logger.py
@@ -1,5 +1,7 @@
 print("Username Logger")
 with open("users.txt","a") as us:
-    entry = input("Enter the Username: ")
-    us.write(entry)
+    entry = int(input("how many username? "))
+    for i in range(entry):
+        username = input(f"Enter the username no {i+1}: ")
+        us.write(username + "\n") # \n for to entry in next line 
     print("Username saved Successfully")
